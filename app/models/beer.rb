8f700aff5c240a334bfc25107713d4ae0df92774
jkarenko <jk@noo.se>
2014-01-23 00:59:48 +0200

    vko2 t1-6

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,18 @@
 class Beer < ActiveRecord::Base
 	belongs_to :brewery
+	has_many :ratings
+
+	def average_rating_old
+		ratings = Rating.select { |r| r.beer_id == id }
+		sum = 0
+		ratings.each do |r| 
+			sum += r.score
+		end
+		sum/ratings.count
+	end
+
+	def average_rating
+		ratings = Rating.select { |r| r.beer_id == id }
+		ratings.inject(0) { |sum, n| sum += n.score } / ratings.count
+	end
 end
