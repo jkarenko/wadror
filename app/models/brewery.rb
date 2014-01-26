c78a40bea4273ed31ee9666becb5fb2f9856c9d9
jkarenko <jk@noo.se>
2014-01-27 00:15:24 +0200

    vko2 t14

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -3,8 +3,10 @@ class Brewery < ActiveRecord::Base
   has_many :ratings, through: :beers
 
   def average_rating
-    ratings = Rating.select { |r| r.beer_id == id }
-    byebug
+    ratings = []
+    beers.each do |b|
+      ratings += Rating.select { |r| r.beer_id == b.id }
+    end
     ratings.inject(0) { |sum, n| sum += n.score } / ratings.count
 
   end
