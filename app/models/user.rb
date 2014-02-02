2be68fcf7332d123fc26f9cefe76e2ee55b5caef
jkarenko <jk@noo.se>
2014-02-02 20:50:41 +0200

    vko3 t13-14

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
 
   validates :password, format: { :with => /(?=.*[a-zA-Z])(?=.*[0-9]).{4,}/ }
 
-  has_many :ratings
+  has_many :ratings, dependent: :destroy
   has_many :beers, through: :ratings
 
 end
