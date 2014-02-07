053e778b1a4ccfe2fd79b6ac40d2532378b65686
jkarenko <jk@noo.se>
2014-02-07 02:14:51 +0200

    vko4 t2

---
+++ spec/models/beer_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe Beer do
+  it "is saved when beer has name and style" do
+    beer = Beer.create name:"foo", style:"beer"
+
+    expect(beer).to be_valid
+    expect(Beer.count).to eq(1)
+  end
+
+  it "is not saved when beer has no name defined" do
+    beer = Beer.create style:"beer"
+
+    expect(beer).to be_invalid
+    expect(Beer.count).to eq(0)
+  end
+
+  it "is not saved when beer has no style defined" do
+    beer = Beer.create name:"foo"
+
+    expect(beer).to be_invalid
+    expect(Beer.count).to eq(0)
+  end
+end
