0aa47f3c625682800932fc87e8168e8fa78d37fe
jkarenko <jk@noo.se>
2014-02-14 22:36:08 +0200

    vko5 t2

---
+++ spec/features/places_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe "Places" do
+  it "if one is returned by the API, it is shown at the page" do
+    BeermappingApi.stub(:places_in).with("kumpula").and_return(
+        [ Place.new(:name => "Oljenkorsi") ]
+    )
+
+    visit places_path
+    fill_in('city', with: 'kumpula')
+    click_button "Search"
+
+    expect(page).to have_content "Oljenkorsi"
+  end
+
+  it "if multiple are returned by the API, all are shown at the page" do
+    BeermappingApi.stub(:places_in).with("kumpula").and_return(
+        [ Place.new(:name => "Oljenkorsi"), Place.new(:name => "Toinenmesta") ]
+    )
+
+    visit places_path
+    fill_in('city', with: 'kumpula')
+    click_button "Search"
+
+    expect(page).to have_content "Oljenkorsi"
+    expect(page).to have_content "Toinenmesta"
+  end
+
+  it "if none are returned by the API, show 'no locations' message" do
+    BeermappingApi.stub(:places_in).with("kumpula").and_return(
+        []
+    )
+
+    visit places_path
+    fill_in('city', with: 'kumpula')
+    click_button "Search"
+
+    expect(page).to have_content "No locations in kumpula"
+  end
+end
